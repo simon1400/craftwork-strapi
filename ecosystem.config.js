73a68318035067efa98ecf51fe07ddef752bdfaa
Your Name <>
2024-07-02 15:02:13 +0200

    change settings

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       user : 'dimi',
       host : ['89.221.216.23'],
       ref  : 'origin/main',
-      repo : 'git@github.com:simon1400/craftwork-strapi-copy.git',
+      repo : 'git@github.com:simon1400/craftwork-strapi.git',
       path : '/home/dimi/app/craftwork/strapi-copy',
       'post-deploy' : 'yarn && yarn build && pm2 reload ecosystem.config.js --env production',
     }
